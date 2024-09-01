a4180c3fa8af8b018be10f3e4b49db0a31876e87
devshDotTech <jaggajasus436@gmail.com>
2024-09-01 18:31:11 +0530

    added the project

---
+++ README.md
@@ -0,0 +1,60 @@
+# Project Title
+
+Task Tracker CLI.
+
+## Description
+
+This tool lets you track your todo/tasks in the cli. With this tool you can 
+1. Add a task.
+2. Update a task.
+3. Update status of the task.
+4. Delete a task.
+5. List all the tasks and add status filters as well.
+
+## Getting Started
+
+### Dependencies
+
+* You will need terminal, with node.js version 16 and above.
+* (optional) To add the to your path so that you can globally you need root privilages. 
+
+### Installing and Executing program
+
+* Open the terminal and clone this repo
+```
+git clone https://github.com/devshDotTech/github-activity.git
+```
+* Go to the cloned folder and run the js file
+```
+node index.js <command>
+```
+Replace the command with 
+1. add
+2. update <id> <new task>
+3. delete <id>
+4. mark-done <id>
+5. mark-progress <id>
+6. list [done | not-done | in-progress]
+
+* (optional) To add to your path
+```
+sudo ln -s /path/to/your/file/file.js /usr/local/bin/task-tracker
+```
+* Then simply execute the task-tracker from anywhere
+```
+task-tracker list
+```
+
+```
+#output
+id: 1   task: hello earth and world              status: not-done
+id: 2   task: hello Asia                         status: not-done
+id: 3   task: hello India                        status: in-progress
+id: 4   task: hello Maharashtra                  status: done
+
+```
+
+## Acknowledgments
+
+Inspiration and Solution to.
+* [roadmap.sh/task-tracker](https://roadmap.sh/projects/task-tracker)
---
+++ index.js
@@ -0,0 +1,124 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'tasks.json');
+
+//console.log(filePath);
+
+const initFile = () => {
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, JSON.stringify({tasks: []}, null, 2));
+  }
+}
+
+const addTask = (task) => {
+  const data = JSON.parse(fs.readFileSync(filePath));
+  const taskId = data.tasks.length + 1;
+  const newTask = {
+    id: taskId,
+    task,
+    isDone: false,
+    inProgress: false,
+  };
+  data.tasks.push(newTask);
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+  console.log(`${task} added successfully with id: ${taskId}`);
+}
+
+const updateTask = (id, task) => {
+  if (!task) console.log("Please enter the updated task");
+  const data = JSON.parse(fs.readFileSync(filePath));
+  const oldTask = data.tasks.find(task => task.id === id);
+  if (!oldTask){
+    console.log("Task does not exist");
+  }
+  oldTask.task = task;
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+  console.log(`${task} updated successfully with id: ${oldTask.id}`);
+}
+
+const markDone = (id) => {
+  const data = JSON.parse(fs.readFileSync(filePath));
+  const task = data.tasks.find(task => task.id === id);
+  task.isDone = true,
+  task.inProgress = false,
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+  console.log(`${task.task} marked done successfully with id: ${task.id}`);
+}
+
+const markProgress = (id) => {
+  const data = JSON.parse(fs.readFileSync(filePath));
+  const task = data.tasks.find(task => task.id === id);
+  task.isDone = false,
+  task.inProgress = true,
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+  console.log(`${task.task} marked in-Progress successfully with id: ${task.id}`);
+}
+
+const deleteTask = (id) => {
+  const data = JSON.parse(fs.readFileSync(filePath));
+  data.tasks = data.tasks.filter(task => task.id !== id);
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+  console.log(`id: ${id} deleted successfully`);
+}
+
+const listTasks = (status) => {
+  const data = JSON.parse(fs.readFileSync(filePath));
+  let tasks;
+  if (status === "done"){
+    tasks = data.tasks.filter((task) => task.isDone === true);
+  }
+  else if (status === "in-progress") {
+    tasks = data.tasks.filter( item => item.inProgress === true);
+  }
+  else if (status === "not-done") {
+    tasks = data.tasks.filter( task => task.isDone === false && task.inProgress === false)
+  }
+  else{
+    tasks = data.tasks;
+  }
+  tasks.forEach(({id, task, isDone, inProgress}) => {
+    let status;
+    if (isDone === false && inProgress === false) status = "not-done"
+    else if (isDone) status = "done";
+    else status = "in-progress";
+    const idCol = 7;
+    const statusCol = 21;
+    const taskCol = 40;
+
+    const idStr = `id: ${id}`.padEnd(idCol);
+    const statusStr = `status: ${status}`.padEnd(statusCol);
+    const taskStr = `task: ${task}`.padEnd(taskCol);
+    console.log(`${idStr} ${taskStr} ${statusStr}`);
+  });
+}
+
+const main = () => {
+  initFile();
+  const [command, ...args] = process.argv.slice(2);
+  switch(command) {
+    case 'add':
+      addTask(args[0]);
+      break;
+    case 'update':
+      updateTask(parseInt(args[0]), args[1]);
+      break;
+    case "mark-in-progress":
+      markProgress(parseInt(args[0]));
+      break;
+    case "mark-done":
+      markDone(parseInt(args[0]));
+      break;
+    case "delete":
+      deleteTask(parseInt(args[0]));
+      break;
+    case "list":
+      listTasks(args[0]);
+      break;
+    default:
+      console.log('Unknown Command');
+  }
+}
+
+main();
